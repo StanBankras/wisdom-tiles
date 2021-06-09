6ddfa6bba8ab8eef80bde6a3dc6e81b9ecc07ac8
Stan Bankras <stanbankras@gmail.com>
2021-06-09 18:52:40 +0200

    Fix canvas blurryness attempt

--- js/modules/canvas.js
+++ js/modules/canvas.js
@@ -1,11 +1,11 @@
 import { configuration } from '../index.js';
 import { checkConfiguration } from './state-manager.js';
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 let canvasWidth = 400;
+const canvas = document.getElementById('canvas');
+const ctx = setupCanvas(canvas);
 
-setTimeout(() => checkCanvasSize(), 50);
+setTimeout(() => checkCanvasSize(), 500);
 
 export default function() {
   const pattern = new Image();
@@ -13,14 +13,39 @@ export default function() {
   pattern.onload = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = configuration.color;
-    ctx.font = '20px filson-pro';
+    ctx.font = `${getFontSize()}px filson-pro`;
 
     drawPattern(pattern);
     if(configuration.quote) {
-      const lines = calculateWrappedText(configuration.quote, canvasWidth - 120, 30, 100, 100);
+      const lines = calculateWrappedText(configuration.quote, canvasWidth * 0.70, 30, 100, 100);
       drawLinesCentered(lines, canvasWidth / 2, canvasWidth / 2)
     }
   }
+}
+
+// https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+function setupCanvas(canvas) {
+  // Get the device pixel ratio, falling back to 1.
+  var dpr = window.devicePixelRatio || 1;
+  console.log(dpr)
+  // Get the size of the canvas in CSS pixels.
+  var rect = canvas.getBoundingClientRect();
+  // Give the canvas pixel dimensions of their CSS
+  // size * the device pixel ratio.
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+  canvasWidth = rect.width * dpr;
+  canvas.style.width = rect.width;
+  canvas.style.width = rect.height;
+  var ctx = canvas.getContext('2d');
+  // Scale all drawing operations by the dpr, so you
+  // don't have to worry about the difference.
+  ctx.scale(dpr, dpr);
+  return ctx;
+}
+
+function getFontSize() {
+  return canvasWidth / 20;
 }
 
 function drawPattern(img) {
@@ -56,31 +81,33 @@ function drawLinesCentered(lines, centerY, centerX) {
   lines.map(line => {
     const lineWidth = ctx.measureText(line.line).width;
     const xPos = centerX - lineWidth / 2;
-    const yPos = line.y + moveY;
-    if(lineWidth > canvasWidth * 0.8) ctx.font = '16px filson-pro';
+    const yPos = line.y + moveY + 10;
     ctx.fillText(line.line, xPos, yPos);
   });
 }
 
 function checkCanvasSize() {
   if(window.innerWidth < 320) {
-    canvasWidth = 280;
-    canvas.width = 280;
-    canvas.height = 280;
+    setCanvasSize(280);
   } else if(window.innerWidth < 360) {
-    canvasWidth = 300;
-    canvas.width = 300;
-    canvas.height = 300;
+    setCanvasSize(300);
   } else if(window.innerWidth < 500) {
-    canvasWidth = 320;
-    canvas.width = 320;
-    canvas.height = 320;
+    setCanvasSize(320);
   } else {
-    canvasWidth = 400;
-    canvas.width = 400;
-    canvas.height = 400;
+    setCanvasSize(400);
   }
   checkConfiguration();
+}
+
+function setCanvasSize(size) {
+  const dpr = window.devicePixelRatio || 1;
+  size = size * dpr;
+  ctx.scale(dpr, dpr);
+  canvasWidth = size;
+  canvas.width = size;
+  canvas.style.width = size;
+  canvas.height = size;
+  canvas.style.height = size;
 }
 
 window.addEventListener('resize', e => checkCanvasSize());
